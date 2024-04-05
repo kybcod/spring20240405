db3943dd98125bae8d85aa58c80eca003fa5d724
kyb <kyb-0215@naver.com>
2024-04-05 16:44:16 +0900

    RequestParam/타입변환

--- src/main/java/com/study/controller/Controller4.java
+++ src/main/java/com/study/controller/Controller4.java
@@ -42,4 +42,61 @@ public class Controller4 {
     }
 
 
+    // /main4/sub3?query=newjeans
+    // /main4/sub3?query=환승연애
+    @RequestMapping("sub3")
+    public void sub3(WebRequest request) {
+        String value = request.getParameter("query");
+
+        if (value != null) {
+            System.out.println(value + " 검색 하기 로직 실행");
+            System.out.println("검색결과 응답하기");
+        }
+
+    }
+
+    // /main4/sub4?city=seoul
+    // /main4/sub4?city=tokyo
+    // /main4/sub4?city=london
+    @RequestMapping("sub4")
+    public void sub4(WebRequest request) {
+        String value = request.getParameter("city");
+
+        if (value != null) {
+            System.out.println(value + " 검색완료");
+        }
+    }
+
+    // /main4/sub5?age=13
+    // /main4/sub5?age=19
+    // /main4/sub5?age=35
+    @RequestMapping("sub5")
+    public void sub5(WebRequest request) {
+        String value = request.getParameter("age");
+        int age = Integer.parseInt(value);
+
+        if (age < 13) {
+            System.out.println("유아용 정보 검색");
+        } else if (age < 19) {
+            System.out.println("청소년 정보 검색");
+        } else {
+            System.out.println("성인 정보 검색");
+        }
+    }
+
+    // /main4/sub6?weight
+    @RequestMapping("sub6")
+    public void sub6(WebRequest request) {
+        String parameter = request.getParameter("weight");
+        double weight = Double.parseDouble(parameter);
+
+        if (weight < 50.0) {
+            System.out.println("저체중");
+        } else if (weight < 90.0) {
+            System.out.println("보통");
+        } else {
+            System.out.println("과체중");
+        }
+    }
+
 }
