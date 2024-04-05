5ddcd1076aad088faedb4e6390915a9893a0c663
kyb <kyb-0215@naver.com>
2024-04-05 15:08:09 +0900

    Controller

---
+++ src/main/java/com/study/controller/Controller03.java
@@ -0,0 +1,22 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main3")
+public class Controller03 {
+
+    // 경로 : /main3/sub1
+    @RequestMapping("sub1")
+    public void method1() {
+        System.out.println("Controller03.method1");
+    }
+
+    // 경로 : /main3/sub2
+    @RequestMapping("sub2")
+    public void method2() {
+        System.out.println("Controller03.method2");
+    }
+
+}
