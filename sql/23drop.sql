34a241fb35abd6e17a10e3cfabb9a8f7490135f2
kyb <kyb-0215@naver.com>
2024-04-25 14:07:25 +0900

    테이블 삭제

---
+++ sql/23drop.sql
@@ -0,0 +1,15 @@
+USE w3schools;
+
+CREATE TABLE table99(
+    co1 int
+);
+
+DROP TABLE table99;
+SELECT * FROM table99;
+
+# DROP TABLE : 테이블 삭제, 매우 주의해서 실행해야 하는 쿼리
+
+CREATE TABLE ta100(
+    col1 int
+);
+DROP TABLE ta100;
