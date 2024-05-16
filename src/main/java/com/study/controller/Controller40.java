b50e16c6df28d81fab1300e60892f07e4dea1a08
kyb <kyb-0215@naver.com>
2024-05-16 10:00:56 +0900

    /api/someurl의 get, post 요청 2

--- src/main/java/com/study/controller/Controller40.java
+++ src/main/java/com/study/controller/Controller40.java
@@ -18,4 +18,14 @@ public class Controller40 {
     public void method2(){
         System.out.println("Controller40.method2");
     }
+
+    @GetMapping("/api/someurl2")
+    public void method3(){
+        System.out.println("Controller40.method3");
+    }
+
+    @PostMapping("/api/someurl2")
+    public void method4(){
+        System.out.println("Controller40.method4");
+    }
 }
