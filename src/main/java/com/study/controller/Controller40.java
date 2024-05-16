9ccfebc211af9e88617d1c86eb0d60e6ff902b8f
kyb <kyb-0215@naver.com>
2024-05-16 10:41:59 +0900

    /api/someurl4의 get, post 요청 처리

--- src/main/java/com/study/controller/Controller40.java
+++ src/main/java/com/study/controller/Controller40.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+
 @Controller
 public class Controller40 {
 
@@ -35,4 +37,25 @@ public class Controller40 {
                         @RequestParam("email") String email){
         System.out.println("id = " + id + ", name = " + name + ", email = " + email);
     }
+
+    @PostMapping("/api/someurl3")
+    public void method6(@RequestParam("id") Integer id,
+                        @RequestParam("city") String city,
+                        @RequestParam("country") String country){
+        System.out.println("id = " + id + ", city = " + city + ", country = " + country);
+    }
+
+    @GetMapping("/api/someurl4")
+    public void method7(Integer id, String name){
+        System.out.println("id = " + id + ", name = " + name);
+    }
+
+    @PostMapping("/api/someurl4")
+    public void method8(String[] foods, String city){
+        System.out.println("foods = " + Arrays.toString(foods) + ", city = " + city);
+    }
+
+
+
+
 }
