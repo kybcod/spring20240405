a3ddfa3613d937b2f80a9b67f0380cff04b73383
celine.yerim <celine.yerim@kakaocorp.com>
2024-05-17 09:39:02 +0900

    자바빈

--- src/main/java/com/study/domain/MyBean411.java
+++ src/main/java/com/study/domain/MyBean411.java
@@ -6,4 +6,8 @@ import lombok.Data;
 public class MyBean411 {
     private String name;
     private Integer age;
+
+    public String getDesc(){
+        return name + "님은 " + age + "살입니다.";
+    }
 }
