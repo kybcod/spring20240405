4556cb7a28a80201869cf83c616284da1d102a32
celine.yerim <celine.yerim@kakaocorp.com>
2024-05-17 09:38:46 +0900

    자바빈 객체 응답3

--- src/main/java/com/study/controller/Controller42.java
+++ src/main/java/com/study/controller/Controller42.java
@@ -1,5 +1,7 @@
 package com.study.controller;
 
+import com.study.domain.MyBean411;
+import com.study.domain.MyBean412;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,5 +47,31 @@ public class Controller42 {
                 "foods", List.of("떡볶이", "순대", "튀김"),
                 "desc", Map.of("team", "토트넘", "location", "런던"),
         "hasAccess", true);
+    }
+
+    @GetMapping("sub5")
+    @ResponseBody
+    public MyBean411 method5(){
+        MyBean411 obj = new MyBean411();
+        obj.setName("홍길동");
+        obj.setAge(22);
+        return obj; // JSON으로 직렬
+    }
+
+    @GetMapping("sub6")
+    @ResponseBody
+    public MyBean412 method6() {
+        MyBean412 myBean412 = new MyBean412();
+        myBean412.setFoods(List.of("감자", "고구마"));
+        myBean412.setHeight(177.7);
+        myBean412.setMarried(false);
+
+        MyBean411 myBean411 = new MyBean411();
+        myBean411.setName("흥민");
+        myBean411.setAge(55);
+        myBean412.setInfo(myBean411);
+        myBean412.setName("강인");
+        return myBean412;
+
     }
 }
