4333a213e46415cb9d132d79677ccdd909a5d950
celine.yerim <celine.yerim@kakaocorp.com>
2024-05-18 08:59:59 +0900

    로그인 여부에 따라 접근 가능한 메소드 작성

--- src/main/java/com/study/controller/Controller44.java
+++ src/main/java/com/study/controller/Controller44.java
@@ -1,10 +1,12 @@
 package com.study.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,10 +33,23 @@ public class Controller44 {
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .issuedAt(Instant.now())
-                .expiresAt(Instant.now().plusSeconds(60*60^24))
+                .expiresAt(Instant.now().plusSeconds(60*60*24))
                 .subject(username)
 //                .claim("scope", scope) //권한
                 .build();
         return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    @GetMapping("all")
+    @ResponseBody
+    public String all() {
+        return "누구나 접근 가능한 경로";
+    }
+
+    @GetMapping("user")
+    @ResponseBody
+    @PreAuthorize("isAuthenticated()")
+    public String user() {
+        return "로그인한 유저만 접근 가능한 경로";
     }
 }
