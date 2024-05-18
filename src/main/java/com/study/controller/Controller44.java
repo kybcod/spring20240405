2ab5cae2c1352dc3c648c7b5c8ecf6a781f42baf
celine.yerim <celine.yerim@kakaocorp.com>
2024-05-18 09:19:29 +0900

    로그인 여부에 따라 접근 가능한 메소드 작성

--- src/main/java/com/study/controller/Controller44.java
+++ src/main/java/com/study/controller/Controller44.java
@@ -6,11 +6,7 @@ import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.util.Map;
@@ -28,14 +24,14 @@ public class Controller44 {
         // 사용자 이름
         String username = map.get("username").toString();
         // 권한
-//        map.get("scope");
+        String scope = map.get("scope").toString();
 
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .issuedAt(Instant.now())
-                .expiresAt(Instant.now().plusSeconds(60*60*24))
+                .expiresAt(Instant.now().plusSeconds(60 * 60 * 24))
                 .subject(username)
-//                .claim("scope", scope) //권한
+                .claim("scope", scope) // 권한
                 .build();
         return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
@@ -50,6 +46,27 @@ public class Controller44 {
     @ResponseBody
     @PreAuthorize("isAuthenticated()")
     public String user() {
-        return "로그인한 유저만 접근 가능한 경로";
+        return "로그인 한 유저만 접근 가능한 경로";
+    }
+
+    @GetMapping("admin")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('SCOPE_admin')")
+    public String admin() {
+        return "어드민만 접근 가능한 경로";
+    }
+
+    @GetMapping("manager")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('SCOPE_manager')")
+    public String manager() {
+        return "매니저만 접근 가능한 경로";
+    }
+
+    @GetMapping("ma")
+    @ResponseBody
+    @PreAuthorize("hasAnyAuthority('SCOPE_manager', 'SCOPE_admin')")
+    public String ma() {
+        return "매니져 또는 어드민이 접근 가능한 경로";
     }
 }
