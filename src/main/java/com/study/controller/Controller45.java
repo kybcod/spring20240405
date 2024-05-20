45c09e82627c38f5ed2b00975b6254b3992a2b36
kyb <kyb-0215@naver.com>
2024-05-20 16:18:16 +0900

    여러 파일 저장

--- src/main/java/com/study/controller/Controller45.java
+++ src/main/java/com/study/controller/Controller45.java
@@ -1,10 +1,7 @@
 package com.study.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -21,6 +18,21 @@ public class Controller45 {
         String path = "C:/Temp/" + file.getOriginalFilename();
         file.transferTo(new File(path));
 
+        return null;
+    }
+
+    @PostMapping("sub2")
+    @ResponseBody
+    public String sub2(String name, @RequestParam("file[]") MultipartFile[] file) throws IOException{
+        System.out.println("name = " + name);
+        if (file == null || file.length > 0){
+            for (MultipartFile file1 : file) {
+                if (file1.getSize() > 0){
+                    String path = "C:/Temp/" + file1.getOriginalFilename();
+                    file1.transferTo(new File(path));
+                }
+            }
+        }
         return null;
     }
 }
